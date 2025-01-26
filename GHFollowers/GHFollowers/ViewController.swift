4135d8ff7c7f42eb56ec5910a407e18dc7139e3c
DavidIvic-TomTom <david.ivic@tomtom.com>
2025-01-26 15:17:15 +0100

    init

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by David on 26.1.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
