8480ccd872301e1a53bd3b2105afc26b5ee78fc3
DavidIvic-TomTom <david.ivic@tomtom.com>
2025-01-26 17:06:37 +0100

    feat: introduce vc files along with custom button and text field

---
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by David on 26.1.25..
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemBlue
+    }
+
+}
